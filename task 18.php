32d81b1d5ccc27b9550ca2ac7da8b415cacced4d
yafaaa <131185124+yafaaa@users.noreply.github.com>
2026-03-16 14:42:23 +0300

    Update task 18.php

--- task 18.php
+++ task 18.php
@@ -1,1 +1,25 @@
 
+<h2>Create Account</h2>
+<form action="register.php" method="POST">
+    <input type="text" name="username" placeholder="Username"><br><br>
+    <input type="email" name="email" placeholder="Email"><br><br>
+    <button type="submit">Register</button>
+</form>
+
+<hr>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['username']) && isset($_POST['email'])) {
+        
+        $user = $_POST['username'];
+        $email = $_POST['email'];
+
+        if (empty($user) || empty($email)) {
+            echo "<p>Error: Both Username and Email are required!</p>";
+        } else {
+            echo "<p '>Success! Welcome, " . htmlspecialchars($user) . ".</p>";
+        }
+    }
+}
+?>
