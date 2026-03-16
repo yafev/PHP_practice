0ef95358e204795c1a3d1482c3f4c0ee9582e14c
YAFET MICHAEL <yafet.michael@aastustudent.edu.et>
2026-03-16 12:25:10 +0300

    Create task 2.php

---
+++ task 2.php
@@ -0,0 +1,12 @@
+<?php
+
+$name = "Yafet";
+$age = 21;
+$department = "Software Engineering";
+
+
+echo "<h1>Student Profile</h1>";
+echo "<b>Name:</b> $name <br>";
+echo "<b>Age:</b>  $age <br>";
+echo "<b>Department:</b>  $department";
+?>
