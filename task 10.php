8ebf98400c3597c1b3c7f04b6a44c26dcff179b2
YAFET MICHAEL <yafet.michael@aastustudent.edu.et>
2026-03-16 13:14:34 +0300

    Create task 10.php

---
+++ task 10.php
@@ -0,0 +1,12 @@
+<?php
+$business = [
+    "name" => "Yaf cafe",
+    "category" => "Software Development",
+    "location" => "Addis Ababa"
+];
+echo "<h2>Business Directory</h2>";
+echo "Name: " . $business["name"] . "<br>";
+echo "Category: " . $business["category"] . "<br>";
+echo "Location: " . $business["location"];
+
+?>
