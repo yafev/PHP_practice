1bcd18802c867df8db80553ff5ab35ddd9f73874
YAFET MICHAEL <yafet.michael@aastustudent.edu.et>
2026-03-16 12:49:22 +0300

    Create task 6.php

---
+++ task 6.php
@@ -0,0 +1,32 @@
+<?php
+
+$today = date("l");
+
+
+switch ($today) {
+    case "Monday":
+        echo "Today is $today!";
+        break;
+    case "Tuesday":
+        echo "Today is $today!";
+        break;
+    case "Wednesday":
+        echo "Today is $today!";
+        break;
+    case "Thursday":
+        echo "Today is $today!";
+        break;
+    case "Friday":
+        echo "Today is $today!";
+        break;
+    case "Saturday":
+        echo "Today is $today!";
+        break;
+    case "Sunday":
+        echo "Today is $today!";
+        break;
+    default:
+        echo "Today is $today!";
+        break;
+}
+?>
